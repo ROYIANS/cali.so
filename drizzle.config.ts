113b8f23462ea540d2fe29b20dde9697b4e1ca09
ROYIANS <1294686101@qq.com>
2023-08-29 14:24:24 +0800

    fix: 修复一些问题

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,5 +6,11 @@ export default {
   driver: 'mysql2',
   schema: './db/schema.ts',
   out: './db/migrations',
-  dbCredentials: { connectionString: process.env.DATABASE_URL || '' },
+  dbCredentials: {
+    user: process.env.DATABASE_USERNAME || '',
+    password: process.env.DATABASE_PASSWORD || '',
+    host: process.env.DATABASE_HOST || '',
+    database: process.env.DATABASE_SCHEMA || '',
+    port: process.env.DATABASE_PORT || 3306,
+  },
 } satisfies Config
